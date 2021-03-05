28d06653226215440616099b288b868838dd331b
SN1CK3T <47250557+SN1CK3T@users.noreply.github.com>
2021-03-05 15:16:51 -0500

    started to implement multithreading

---
+++ failed version/NinjaTurtles/NTbutinCS/check.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Diagnostics.Tracing.Parsers.MicrosoftWindowsWPF;
+using System;
+using System.Diagnostics;
+
+namespace NTbutinCS
+{
+    internal class check
+    {
+        private int pID;
+        private Process target;
+        private bool tgt = true;
+
+        public check(int pID)
+        {
+            this.pID = pID;
+            try
+            {
+                Process target = Process.GetProcessById(pID);
+                this.target = target;
+            }
+            catch
+            {
+                Console.WriteLine("Couldnt parse");
+                tgt = false;
+            } 
+        }
+
+        public void NT()
+        {
+            Console.WriteLine("Process Started: " + pID);
+            
+            if (tgt)
+            {
+                Console.WriteLine("[{0}]", string.Join(", ", target.Modules));
+                Console.WriteLine(target.Modules);
+            }
+
+            
+        }
+    }
+}
---
+++ failed version/NinjaTurtles/NinjaTurtles.py
@@ -0,0 +1,52 @@
+import time 
+import ptrace.debugger
+import signal
+import subprocess
+import sys
+import os
+from watchdog.observers import Observer 
+from watchdog.events import FileSystemEventHandler 
+  
+  
+class OnMyWatch: 
+    # Set the directory on watch 
+    watchDirectory = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+  
+    def __init__(self): 
+        self.observer = Observer() 
+  
+    def run(self): 
+        event_handler = Handler() 
+        self.observer.schedule(event_handler, self.watchDirectory, recursive = True) 
+        self.observer.start() 
+        try: 
+            while True: 
+                time.sleep(5) 
+        except: 
+            self.observer.stop() 
+            print("Observer Stopped") 
+  
+        self.observer.join() 
+  
+  
+class Handler(FileSystemEventHandler): 
+  
+    @staticmethod
+    def on_any_event(event): 
+        if event.is_directory: 
+            return None
+  
+        elif event.event_type == 'created': 
+            # Event is created, you can process it now 
+            print("Watchdog received created event - % s." % event.src_path) 
+        elif event.event_type == 'modified': 
+            # Event is modified, you can process it now 
+            print("Watchdog received modified event - % s." % event.src_path) 
+              
+  
+if __name__ == '__main__': 
+   #watch = OnMyWatch() 
+   #watch.run()
+    d = ptrace.debugger.PtraceDebugger()
+    process = d.addProcess(21200, False)
+    process.is_stopped
